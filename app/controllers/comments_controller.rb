10528a53213e859c1009850e821b200fd17e543e
tommy-pellerin <tommytp@hotmail.fr>
2024-02-14 20:01:24 +0100

    fin de la partie commentaire

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,5 +30,42 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    puts "%"*50
+    puts "Lorsque je suis dans l'edit du commentaire :"
+    puts params    
+    puts "%"*50
+    @comment = Comment.find(params[:id])
+    @gossip = Gossip.find(params[:gossip_id])
+  end
+
+  def update    
+    puts "%"*50
+    puts "Lorsque je soumet la modofication du commentaire :"
+    puts params 
+    comment_params = params.require(:comment).permit(:content)
+    @comment = Comment.find(params[:id])
+    @gossip = Gossip.find(params[:gossip_id])
+    puts "%"*50
+    if @comment.update(content:comment_params[:content],user:@gossip.user,gossip:@gossip)
+      redirect_to @gossip
+    else
+      render "gossip/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    puts "%"*50
+    puts "Lorsque je soumet la suppression du commentaire :"
+    puts params     
+    puts "%"*50
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    puts "le commentaire id :#{params[:id]} a été supprimé"
+    redirect_to @gossip
+  end
+
+
   
 end
